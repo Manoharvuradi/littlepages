1da669e6e2cf94783d06ff4edac4d9880c059424
Manoharvuradi <manoharvuradi5@gmail.com>
2026-01-21 12:39:53 +0530

    cored check

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -8,7 +8,8 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-      process.env.FRONTEND_URL
+      process.env.FRONTEND_URL || 'http://localhost:3000',
+      'http://localhost:4000'
     ],
     credentials: true,
   });
